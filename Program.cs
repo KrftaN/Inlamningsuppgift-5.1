ef53d1d9b8eec129a81a95619ccfb01cdb558062
Admin <Admin@TEB123021>
2023-11-22 10:04:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sentences = { "Detta är den första meningen.", "Och detta är den andra meningen", "Detta är den avslutande meningen" };
+
+        foreach (var element in sentences)
+        {
+            Console.WriteLine($"{element}\n");
+        }
+    }
+}
